aabf4352e7d17059603a0610305bb30eddc6be21
mrnavis <navifunk.is@gmail.com>
2025-10-22 15:18:54 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+src/main.jsx
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './tikcoin/TikCoinTrainer.jsx'
+import './styles.css'
+
+createRoot(document.getElementById('root')).render(<App />)
