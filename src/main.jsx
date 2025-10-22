8224c10c80fe062becc03630bce779ddbca542db
mrnavis <navifunk.is@gmail.com>
2025-10-22 17:54:36 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-// OJO: sin extensión para evitar conflictos de resolución
-import App from './tikcoin/TikCoinTrainer'
+import App from './App.jsx'
 import './styles.css'
 
 createRoot(document.getElementById('root')).render(<App />)
